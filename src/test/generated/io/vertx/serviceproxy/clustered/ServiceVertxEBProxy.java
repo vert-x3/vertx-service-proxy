34977fcc14ffe6759a99ab177422b3726c7068de
Julien Viet <julien@julienviet.com>
2016-09-12 07:00:13 +0200

    Releasing 3.3.3

--- src/test/generated/io/vertx/serviceproxy/clustered/ServiceVertxEBProxy.java
+++ src/test/generated/io/vertx/serviceproxy/clustered/ServiceVertxEBProxy.java
@@ -47,6 +47,7 @@ import io.vertx.core.Handler;
   Generated Proxy code - DO NOT EDIT
   @author Roger the Robot
 */
+@SuppressWarnings({"unchecked", "rawtypes"})
 public class ServiceVertxEBProxy implements Service {
 
   private Vertx _vertx;
--- src/test/generated/io/vertx/serviceproxy/clustered/ServiceVertxProxyHandler.java
+++ src/test/generated/io/vertx/serviceproxy/clustered/ServiceVertxProxyHandler.java
@@ -54,6 +54,7 @@ import io.vertx.core.Handler;
   Generated Proxy code - DO NOT EDIT
   @author Roger the Robot
 */
+@SuppressWarnings({"unchecked", "rawtypes"})
 public class ServiceVertxProxyHandler extends ProxyHandler {
 
   public static final long DEFAULT_CONNECTION_TIMEOUT = 5 * 60; // 5 minutes 
