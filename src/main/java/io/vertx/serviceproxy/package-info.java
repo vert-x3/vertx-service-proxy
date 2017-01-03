299dc3f17509ce964550933c74d9af90d301d296
Thomas Segismont <tsegismo@redhat.com>
2017-01-03 18:42:57 +0100

    Fixes #46 Missing dependency in the doc for implementing service proxies

--- src/main/java/io/vertx/serviceproxy/package-info.java
+++ src/main/java/io/vertx/serviceproxy/package-info.java
@@ -30,7 +30,7 @@
  *
  * == Using Vert.x service proxies
  *
- * To use the Vert.x Service Proxies, add the following dependency to the _dependencies_ section of
+ * To *use* Vert.x Service Proxies, add the following dependency to the _dependencies_ section of
  * your build descriptor:
  *
  * * Maven (in your `pom.xml`):
@@ -49,6 +49,27 @@
  * [source,groovy,subs="+attributes"]
  * ----
  * compile '${maven.groupId}:${maven.artifactId}:${maven.version}'
+ * ----
+ *
+ * To *implement* service proxies, also add:
+ *
+ * * Maven (in your `pom.xml`):
+ *
+ * [source,xml,subs="+attributes"]
+ * ----
+ * <dependency>
+ *   <groupId>${maven.groupId}</groupId>
+ *   <artifactId>vertx-codegen</artifactId>
+ *   <version>${maven.version}</version>
+ *   <scope>provided</scope>
+ * </dependency>
+ * ----
+ *
+ * * Gradle (in your `build.gradle` file):
+ *
+ * [source,groovy,subs="+attributes"]
+ * ----
+ * compileOnly '${maven.groupId}:vertx-codegen:${maven.version}'
  * ----
  *
  * Be aware that as the service proxy mechanism relies on code generation, so modifications to the _service interface_
