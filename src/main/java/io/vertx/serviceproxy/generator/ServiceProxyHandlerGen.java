9ad9857fa9adff224425d9d5ca1c844af9202ae1
slinkydeveloper <francescoguard@gmail.com>
2019-06-06 12:00:42 +0200

    Added additional entrypoints for web api service
    Signed-off-by: slinkydeveloper <francescoguard@gmail.com>

--- src/main/java/io/vertx/serviceproxy/generator/ServiceProxyHandlerGen.java
+++ src/main/java/io/vertx/serviceproxy/generator/ServiceProxyHandlerGen.java
@@ -60,6 +60,8 @@ public class ServiceProxyHandlerGen extends Generator<ProxyModel> {
     return model.getIfaceSimpleName() + "VertxProxyHandler";
   }
 
+  public Stream<String> additionalImports() { return Stream.empty(); }
+
   @Override
   public String render(ProxyModel model, int index, int size, Map<String, Object> session) {
     StringWriter buffer = new StringWriter();
@@ -70,7 +72,7 @@ public class ServiceProxyHandlerGen extends Generator<ProxyModel> {
     writer.newLine();
     utils.writeImport(writer, model.getIfaceFQCN());
     utils.handlerGenImports(writer);
-    utils.additionalImports(model).forEach(i -> utils.writeImport(writer, i));
+    Stream.concat(utils.additionalImports(model), additionalImports()).distinct().forEach(i -> utils.writeImport(writer, i));
     utils.roger(writer);
     writer
       .code("@SuppressWarnings({\"unchecked\", \"rawtypes\"})\n")
