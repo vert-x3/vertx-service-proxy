89668c66552e840d873180be392054ce6734796d
Julien Viet <julien@julienviet.com>
2024-06-14 21:18:22 +0200

    Update to vertx-auth split pkg remediation

--- src/main/java/io/vertx/serviceproxy/ServiceAuthInterceptor.java
+++ src/main/java/io/vertx/serviceproxy/ServiceAuthInterceptor.java
@@ -21,7 +21,7 @@ import io.vertx.core.eventbus.Message;
 import io.vertx.core.eventbus.ReplyException;
 import io.vertx.core.eventbus.ReplyFailure;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.auth.User;
+import io.vertx.ext.auth.user.User;
 import io.vertx.ext.auth.authentication.AuthenticationProvider;
 import io.vertx.ext.auth.authentication.CredentialValidationException;
 import io.vertx.ext.auth.authentication.TokenCredentials;
